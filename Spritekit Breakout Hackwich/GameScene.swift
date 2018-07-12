9f99b3ed5ee2f0a05c0d8358725fcab1c9b707f3
Jean Cho <jeancho@jeans-macbook-air.local>
2018-07-12 10:20:03 -0500

    created dynamic paddle behavior

--- Spritekit Breakout Hackwich/GameScene.swift
+++ Spritekit Breakout Hackwich/GameScene.swift
@@ -104,6 +104,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(loseZone)
     }
     
+    //when you first touch the paddle
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x //only x so the paddle can't move veritcally
+        }
+    }
+    
+    //as you keep touching the paddle
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x //only x so the paddle can't move veritcally
+        }
+    }
+    
+    
  
 }
 
