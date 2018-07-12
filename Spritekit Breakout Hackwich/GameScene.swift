892ac01b832c842ccf1894c6e71ae04c5fc56458
Jean Cho <jeancho@jeans-macbook-air.local>
2018-07-12 09:53:41 -0500

    created background

--- Spritekit Breakout Hackwich/GameScene.swift
+++ Spritekit Breakout Hackwich/GameScene.swift
@@ -12,9 +12,24 @@ import GameplayKit
 class GameScene: SKScene {
    
     override func didMove(to view: SKView) {
-       
+       createBackground()
     }
     
-    
+    //Background
+    func createBackground () {
+        let stars = SKTexture(imageNamed: "stars")
+        for i in 0...1 {
+            let starsBackground = SKSpriteNode(texture: stars)
+            starsBackground.zPosition = -1
+            starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
+            addChild(starsBackground)
+            //below: adding a second view of the stars background below the actual screen and moving it up forever ; background isn't actually a moving gif
+            let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            starsBackground.run(moveForever)
+        }
+    }
  
 }
